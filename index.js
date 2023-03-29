49ecaf2e50ded57011a741bd8be3d5054880a9ea
danieljuma5 <jumadaniel275@gmail.com>
2023-03-29 23:14:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Create an empty set to store the seen numbers
+  const seen = new Set();
+
+  // Loop through the array
+  for (let i = 0; i < array.length; i++) {
+    // Check if the complement of the current number is in the seen set
+    const complement = target - array[i];
+    if (seen.has(complement)) {
+      return true;
+    }
+    // Add the current number to the seen set
+    seen.add(array[i]);
+  }
+
+  // If no pair adds up to the target, return false
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+
 */
 
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the end of the array
+  compare the number we're iterating over to the corresponding number at the end of the array
+    if the letters add up to target, return false
+
+if any two numbers add up to target, return true
 */
 
 /*
@@ -17,6 +39,10 @@ function hasTargetSum(array, target) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+console.log("Expecting: true");
+console.log("=>",hasTargetSum([1,2,3,4],5));
+
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
